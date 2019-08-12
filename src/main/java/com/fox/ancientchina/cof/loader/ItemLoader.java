101943eeb19509fb9c10c729cd0fe394cde56c12
ajacker <lj456852753951>
2019-08-12 16:03:14 +0800

    初始化cof部分的工作

---
+++ src/main/java/com/fox/ancientchina/cof/loader/ItemLoader.java
@@ -0,0 +1,15 @@
+package com.fox.ancientchina.cof.loader;
+
+import com.fox.ancientchina.cof.item.NyaApple;
+import net.minecraft.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ajacker
+ */
+public class ItemLoader {
+    public static final List<Item> ITEMS = new ArrayList<>();
+    public static final Item NYA_APPLE = new NyaApple("nya_apple");
+}
---
+++ src/main/java/com/fox/ancientchina/cof/util/handler/RegistryHandler.java
@@ -0,0 +1,46 @@
+package com.fox.ancientchina.cof.util.handler;
+
+import com.fox.ancientchina.cof.loader.BlockLoader;
+import com.fox.ancientchina.cof.loader.ItemLoader;
+import com.fox.ancientchina.core.util.IModelRegister;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static com.fox.ancientchina.cof.AncientChina_COF.MODID;
+
+
+/**
+ * @author ajacker
+ */
+@Mod.EventBusSubscriber(modid = MODID)
+public class RegistryHandler {
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(ItemLoader.ITEMS.toArray(new Item[0]));
+    }
+
+    @SubscribeEvent
+    public static void onBlockRegister(RegistryEvent.Register<Block> event) {
+        event.getRegistry().registerAll(BlockLoader.BLOCKS.toArray(new Block[0]));
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        for (Item item : ItemLoader.ITEMS) {
+            if (item instanceof IModelRegister) {
+                ((IModelRegister) item).registerModels();
+            }
+        }
+
+        for (Block block : BlockLoader.BLOCKS) {
+            if (block instanceof IModelRegister) {
+                ((IModelRegister) block).registerModels();
+            }
+        }
+
+    }
+}
