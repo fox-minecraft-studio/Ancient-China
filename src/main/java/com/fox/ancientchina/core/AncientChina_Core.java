979033ca670ccf7b81d1ca57a7dbdea4f86f019f
kualigh <18177114151@163.com>
2019-08-13 21:20:13 +0800

    快速配置

--- src/main/java/com/fox/ancientchina/core/AncientChina_Core.java
+++ src/main/java/com/fox/ancientchina/core/AncientChina_Core.java
@@ -17,17 +17,14 @@ public class AncientChina_Core {
     public static final String MODID = "ancientchina-core";
     public static final String NAME = "Ancient China(Core)";
     public static final String VERSION = "0.0.1";
-<<<<<<< HEAD:src/main/java/com/fox/ancientchina/core/AncientChina.java
+
 
     @Instance(MODID)
-    public static AncientChina INSTANCE;
+    public static AncientChina_Core INSTANCE;
 
-    @SidedProxy(clientSide = "com.fox.ancientchina.core.client.ClientProxy",
-            serverSide = "com.fox.ancientchina.core.common.CommonProxy")
-=======
     @SidedProxy(clientSide = "com.fox.ancientchina.core.proxy.ClientProxy",
             serverSide = "com.fox.ancientchina.core.proxy.CommonProxy")
->>>>>>> 554ef93281d7ea0d9460025b37ec38c56cebab99:src/main/java/com/fox/ancientchina/core/AncientChina_Core.java
+
     public static CommonProxy proxy;
 
     @EventHandler
