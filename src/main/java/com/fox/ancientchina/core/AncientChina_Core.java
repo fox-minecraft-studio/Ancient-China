22f84ff79be3d7e731b2368673ab49175998943f
kualigh <18177114151@163.com>
2019-08-15 12:05:37 +0800

    为mod添加logger

--- src/main/java/com/fox/ancientchina/core/AncientChina_Core.java
+++ src/main/java/com/fox/ancientchina/core/AncientChina_Core.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author ajacker
@@ -20,7 +21,6 @@ public class AncientChina_Core {
     public static final String NAME = "Ancient China(Core)";
     public static final String VERSION = "0.0.1";
 
-
     @Instance(MODID)
     public static AncientChina_Core INSTANCE;
 
@@ -29,9 +29,12 @@ public class AncientChina_Core {
 
     public static CommonProxy proxy;
 
+    private Logger logger;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
+        logger = event.getModLog();
     }
 
     @EventHandler
@@ -43,5 +46,9 @@ public class AncientChina_Core {
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
+    }
+
+    public Logger getLogger(){
+        return logger;
     }
 }
