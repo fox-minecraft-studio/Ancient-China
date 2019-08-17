3b752cef7e4877b12d3484e40b6c5986b3fad0b2
kualigh <18177114151@163.com>
2019-08-17 19:53:42 +0800

    cia初始化，但目前懒得添加任何物品

---
+++ src/main/java/com/fox/ancientchina/cia/lib/ModInfo.java
@@ -0,0 +1,10 @@
+package com.fox.ancientchina.cia.lib;
+
+public class ModInfo {
+    public static final String MODID = "ancientchina-cia";
+    public static final String NAME = "Ancient China(Chemistry In Alchemy)";
+    public static final String VENSION = "@vension-cia";
+
+    public static final String CLIENT_SIDE = "com.fox.ancientchina.cia.proxy.ClientProxy";
+    public static final String SERVER_SIDE = "com.fox.ancientchina.cia.proxy.CommonProxy";
+}
---
+++ src/main/java/com/fox/ancientchina/cia/proxy/CommonProxy.java
@@ -0,0 +1,22 @@
+package com.fox.ancientchina.cia.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * @author yaesey
+ */
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    public void Init(FMLInitializationEvent event){
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
